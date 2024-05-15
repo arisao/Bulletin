5f6382bb4a749c6c2070e182d54814475ddd683e
arisao <arisaohta@172.30.1.45>
2024-05-15 12:29:53 +0900

    CORSエラー解決　.allowedOrigins　port8800を許容。

--- src/main/java/com/bulletin/config/webMvcConfig.java
+++ src/main/java/com/bulletin/config/webMvcConfig.java
@@ -9,7 +9,7 @@ public class webMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081");
+                .allowedOrigins("http://localhost:8800");
     }
 }
 
